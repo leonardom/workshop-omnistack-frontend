cf5787b28bd2ff6502556cc28eebf807f1148380
Leonardo Marcelino <leonardo.marcelino@gmail.com>
2019-06-06 21:31:59 -0300

    Updates

---
+++ precache-manifest.e5f85c8168734eec11fe0f858bab7191.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "505434d291eaf5c6921cbae4c4d7355c",
+    "url": "/workshop-omnistack-frontend/index.html"
+  },
+  {
+    "revision": "0ef0a84c486f3f2d080d",
+    "url": "/workshop-omnistack-frontend/static/css/main.b9d3997c.chunk.css"
+  },
+  {
+    "revision": "0fbf26bc092b7c1ddf99",
+    "url": "/workshop-omnistack-frontend/static/js/2.e3379c23.chunk.js"
+  },
+  {
+    "revision": "0ef0a84c486f3f2d080d",
+    "url": "/workshop-omnistack-frontend/static/js/main.f0ebe8f8.chunk.js"
+  },
+  {
+    "revision": "2f28ec0acc857b0376e7",
+    "url": "/workshop-omnistack-frontend/static/js/runtime~main.3f748d3a.js"
+  }
+]);
